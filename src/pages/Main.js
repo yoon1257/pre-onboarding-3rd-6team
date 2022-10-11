d2c3af3638fe70222d2766b021ab453318b212a9
yuna <yureve5772@gmail.com>
2022-10-11 19:34:03 +0900

    Add: 초기셋팅

---
+++ src/pages/Main.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Main = (props) => {
+  return <div>main</div>;
+};
+export default Main;
---
+++ src/styles/theme.js
@@ -0,0 +1,2 @@
+// ${({theme}) => theme.gray}; <= 이런식으로 적어주세요
+export const gray = "#eee";
