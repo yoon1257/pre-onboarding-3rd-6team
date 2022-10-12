77c15fd71f72e157d09494595e31e36c4f181265
jeong-yewon <jdc209802@gmail.com>
2022-10-13 03:18:45 +0900

    Add: 녹음기 레이아웃 첫번째 화면 완성

---
+++ src/components/recordUI/PlayControlBox.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+import { BiReset, BiDownArrowCircle } from 'react-icons/bi';
+
+const PlayControlBox = () => {
+  return (
+    <StyledControlBox>
+      <OptionBtn>
+        <BiReset />
+      </OptionBtn>
+      <div className='record-start'></div>
+      <OptionBtn>
+        <BiDownArrowCircle />
+      </OptionBtn>
+    </StyledControlBox>
+  );
+};
+
+const StyledControlBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 30vh;
+  margin: auto 0;
+
+  .record-start {
+    text-align: center;
+    align-items: center;
+    width: 4em;
+    height: 4em;
+    background-color: #05aac6;
+    border-radius: 50px;
+  }
+`;
+
+const OptionBtn = styled.div`
+  display: flex;
+  align-items: center;
+
+  svg {
+    font-size: 4em;
+    color: #c2c2c2;
+  }
+`;
+export default PlayControlBox;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,81 @@
-import React from 'react';
-import Record from '../components/Record';
+import React, { useRef, useState } from 'react';
+import MovingBar from '../components/recordUI/MovingBar';
+import styled, { css } from 'styled-components';
+import logo from '../asset/images/logo.png';
+import waveSoundPlay from '../asset/images/wave-sound-blue.png';
+import waveSoundStop from '../asset/images/wave-sound-sky.png';
 
 const Main = (props) => {
   return (
-    <div>
-      <Record />
-    </div>
+    <RecorderContainer>
+      <header>
+        <Logo>
+          <img src={logo} alt='logo' />
+          <span>Audio Recorder</span>
+        </Logo>
+      </header>
+      <main className='sound-status-screen'>
+        <img className='sound-wave' src={waveSoundPlay} alt='sound-wave' />
+        <div className='sound-time'></div>
+      </main>
+      <MovingBar />
+    </RecorderContainer>
   );
 };
+
+const FlexCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const RecorderContainer = styled.div`
+  height: 100vh;
+  overflow-y: hidden;
+
+  @media screen and (min-width: 600px) {
+    font-size: 20px;
+  }
+
+  @media screen and (min-width: 1100px) {
+    font-size: 22px;
+  }
+
+  header {
+    padding: 0.6em 0;
+    box-shadow: 0 3px 10px 0 rgba(32, 32, 32, 0.08);
+  }
+
+  .sound-status-screen {
+    ${FlexCenter};
+    height: 55vh;
+
+    .sound-wave {
+      width: 11.8em;
+    }
+  }
+
+  .move {
+    transform: translateY(-55vh);
+    transition: all 0.3s ease-in;
+  }
+`;
+
+export const Logo = styled.div`
+  ${FlexCenter};
+  flex-direction: column;
+
+  img {
+    width: 4.7em;
+  }
+
+  span {
+    padding: 3px 9px;
+    border-radius: 0.8em;
+    background-color: #05aac6;
+    color: white;
+    font-size: 0.75em;
+  }
+`;
+
 export default Main;
