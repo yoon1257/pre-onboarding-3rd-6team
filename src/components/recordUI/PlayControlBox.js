371dce2372d0896b77d32b4f3db287105d666767
jeong-yewon <jdc209802@gmail.com>
2022-10-13 10:11:18 +0900

    Modify: 재생 컨트롤 버튼 img에서 svg로 변경

--- src/components/recordUI/PlayControlBox.js
+++ src/components/recordUI/PlayControlBox.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { BiReset, BiDownArrowCircle } from 'react-icons/bi';
+import { BsCircleFill, BsStopCircle, BsPlayCircle, BsPauseCircleFill } from 'react-icons/bs';
 
 const PlayControlBox = () => {
+  const [recordStatus, setStatus] = useState('standBy');
+
   return (
     <StyledControlBox>
       <OptionBtn>
         <BiReset />
       </OptionBtn>
-      <div className='record-start'></div>
+      <button className='play-control-btn'>
+        <BsCircleFill />
+      </button>
+      {/* <button className='record-start'></button> */}
       <OptionBtn>
         <BiDownArrowCircle />
       </OptionBtn>
@@ -23,19 +29,35 @@ const StyledControlBox = styled.div`
   height: 30vh;
   margin: auto 0;
 
+  .play-control-btn {
+    display: flex;
+    text-align: center;
+    align-items: center;
+    font-size: 3em;
+    color: #05aac6;
+    background-color: #f5f5f5;
+    border: none;
+    cursor: pointer;
+  }
+
   .record-start {
     text-align: center;
     align-items: center;
     width: 4em;
     height: 4em;
     background-color: #05aac6;
+    border: none;
     border-radius: 50px;
+    cursor: pointer;
   }
 `;
 
-const OptionBtn = styled.div`
+const OptionBtn = styled.button`
   display: flex;
   align-items: center;
+  border: none;
+  background-color: #f5f5f5;
+  cursor: pointer;
 
   svg {
     font-size: 3em;
