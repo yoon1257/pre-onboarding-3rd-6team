7045464dfcb747854f906eed61602e4fdb98def3
yuna <yureve5772@gmail.com>
2022-10-14 09:40:20 +0900

    Modify: themepovider 제거

--- src/Router.js
+++ src/Router.js
@@ -2,19 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Context from './contexts/Context';
 import Main from '../src/pages/Main';
-import { ThemeProvider } from 'styled-components';
 
 const Router = (props) => {
   return (
-    <Context>
-      <ThemeProvider>
-        <BrowserRouter basename='pre-onboarding-3rd-6team'>
-          <Routes>
-            <Route path='/' element={<Main />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </Context>
+    <BrowserRouter basename='pre-onboarding-3rd-6team'>
+      <Context>
+        <Routes>
+          <Route path='/' element={<Main />} />
+        </Routes>
+      </Context>
+    </BrowserRouter>
   );
 };
 
